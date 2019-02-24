3a44676ccf5f5d6c303ce0d8fd76484343fa5eb2
FirstWisdom <jason.zelaya21@yahoo.com>
2019-02-24 05:43:57 -0800

    established connection differently

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 import pymongo
+import config
 import os
 
 # Establish connection
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 app = Flask(__name__)
 
